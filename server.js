61bbd6f9f3f7f1702d53cd5391b1c238d8c5c01b
Archit Jain <54016441+Architjain128@users.noreply.github.com>
2022-04-20 04:48:41 +0530

    added scraper instance in server file

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const db = require('./database')
 const DataSchema = require('./schema')
 db.once('open', () => console.log("connected to db"))
 db.on('error', () => console.error('oops cannot connect to db'))
+
+const StackOverflowScraper = require('./stackOverflowScraper.js');
+const Scraper = new StackOverflowScraper({concurrencyCount:5,urlPerPage:15,maxPage:5});
+Scraper.run();
